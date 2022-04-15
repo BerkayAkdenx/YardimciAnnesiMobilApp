595582bd11e5058e93bd1f15948826ef8b13c358
İsmet Berkay Akdeniz <74598142+BerkayAkdenx@users.noreply.github.com>
2022-04-15 18:27:41 +0300

    yardimciAnneler.dart dosyasındaki liste card yapıları düzenlendi.

--- lib/yardimciAnneler.dart
+++ lib/yardimciAnneler.dart
@@ -16,34 +16,6 @@ class _yardimciAnnelerState extends State<yardimciAnneler> {
           clipBehavior: Clip.antiAlias,
           child: Column(
             children: [
-              ListTile(
-                leading: Icon(Icons.arrow_drop_down_circle),
-                title: const Text('AYŞE AKYOL'),
-                subtitle: Text(
-                  'YEMEK/ÜTÜ/ÇAMAŞIR/TEMİZLİK',
-                  style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  'Merhaba 52 yaşında 1 polis ve 1 avukat annesi olma guruyla ev hanımıyım uzun yıllara dayanan tecrübelerimle herzaman yardıma etmeye hazırım.',
-                  style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                ),
-              ),
-              ButtonBar(
-                alignment: MainAxisAlignment.start,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      AlertDialog(
-                          title: Text("Yardimci Annesinin No: "),
-                          content: Text("+90506979745"));
-                    },
-                    child: const Text('Whatsapp Numarası'),
-                  ),
-                ],
-              ),
               Image.asset('resimler/hande.jpg'),
               ListTile(
                 leading: Icon(Icons.arrow_drop_down_circle),
@@ -73,6 +45,14 @@ class _yardimciAnnelerState extends State<yardimciAnneler> {
                   ),
                 ],
               ),
+
+            ],
+          ),
+        ),
+        Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            children: [
               Image.asset('resimler/karin.jpg'),
               ListTile(
                 leading: Icon(Icons.arrow_drop_down_circle),
@@ -102,7 +82,44 @@ class _yardimciAnnelerState extends State<yardimciAnneler> {
                   ),
                 ],
               ),
+
+            ],
+          ),
+        ),
+        Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            children: [
               Image.asset('resimler/asuman.jpg'),
+              ListTile(
+                leading: Icon(Icons.arrow_drop_down_circle),
+                title: const Text('AYŞE AKYOL'),
+                subtitle: Text(
+                  'YEMEK/ÜTÜ/ÇAMAŞIR/TEMİZLİK',
+                  style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'Merhaba 52 yaşında 1 polis ve 1 avukat annesi olma guruyla ev hanımıyım uzun yıllara dayanan tecrübelerimle herzaman yardıma etmeye hazırım.',
+                  style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                ),
+              ),
+              ButtonBar(
+                alignment: MainAxisAlignment.start,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      AlertDialog(
+                          title: Text("Yardimci Annesinin No: "),
+                          content: Text("+90506979745"));
+                    },
+                    child: const Text('Whatsapp Numarası'),
+                  ),
+                ],
+              ),
+
             ],
           ),
         ),
